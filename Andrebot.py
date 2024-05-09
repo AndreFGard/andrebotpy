533abd274694ee450d062bd4fccc5ebc0ad3f3f0
AndreFGard <andrefsagh@gmail.com>
2024-05-08 22:26:49 -0300

    move functions to andrebot.py module

---
+++ Andrebot.py
@@ -0,0 +1,304 @@
+#!/usr/bin/python3
+from random import randint, choice, shuffle, sample
+import math
+import sys
+from sys import argv
+from cmath import cos
+from os import environ
+import json
+import datetime
+from resources import words5, wordleClass
+from requests import get
+
+
+
+class andrebot_message:
+    def __init__(self, data:dict):
+        self.data = data
+
+class Interface:
+    def __init__(self):
+        ...
+    def build_message(message):
+        ...
+    def command(fun):
+        #changeme
+        def new_fun(*args, **kwargs):
+            return fun
+        return new_fun
+
+
+
+class Andrebot:
+    def __init__(self, interface: Interface):
+        self.interface = interface
+
+        # BASE_URL= "https://xinga-me.appspot.com/api"
+        self.emojiappended = ('😀', ' ', '😃', '😄', '😁', '😆', '😅', '😂', '🤣', '🥲', '☺', '️', '😊', '😇', '🙂', '🙃', '😉', '😌', '😍', '🥰', '😘', '😗', '😙', '😚', '😋', '😛', '😝', '😜', '🤪', '🤨',
+                        '🧐', '🤓', '😎', '🥸', '🤩', '🥳', '😏', '😒', '😞', '😔', '😟', '😕', '🙁',
+                        '☹', '😣', '😖', '😫', '😩', '��', '😢', '😭', '😤', '😠', '��', '🤬', '🤯', '😳', '🥵', '��', '😱', '😨', '😰', '😥', '��', '🤗', 
+                        '🤔', '🤭', '🤫', '��', '😶', '😐', '😑', '😬', '��', '😯', '😦', '😧', '😮', '��', '🥱', '😴', '🤤', '😪', '��', '🤐', '🥴', '🤢', '🤮', 
+                        '��', '😷', '🤒', '🤕', '🤑', '��', '😈', '👿', '👹', '👺', '��', '💩', '👻', '💀', '☠', '👽', '👾', '🤖', '🎃', '😺', '😸', '😹', '😻', '😼', 
+                        '😽', '🙀', '😿', '😾')
+        self.indtratadas = open("indtratadas.txt").readlines()
+        from toxingar import curses
+        self.curses = curses
+        # curses is a tuple containing small offenses
+        self.gabaritoh = open("2022_D1.txt").readlines()
+        self.gabaritoe = open("2022_D2.txt").readlines()
+
+        try:
+            apiKey = environ["apiKey"] 
+            apiHeaders = {'Authorization': apiKey}
+            postUrl = environ["postUrl"]
+            getUrl = environ["getUrl"]
+            global testUrl
+            testUrl = environ["testUrl"]
+        except KeyError:
+            print("check apikey, headers, posturl, geturl and testUrl")
+            sys.exit(1)
+        print(f"post url, geturl: {postUrl}, {getUrl}")
+        testRequest = get(testUrl, headers=apiHeaders)
+        print(testRequest.text + ":" + str(testRequest.status_code))
+        if testRequest.status_code != 200:
+            sys.exit(1)
+        self.wordle5 = wordleClass(5, words5, postUrl=postUrl, getUrl=getUrl, testUrl=testUrl, apiHeaders=apiHeaders)
+
+
+    async def add(self, context, left: int, right: int):
+        """Adds two numbers together."""
+        return await context.send(left + right)
+        return await context.send(self, context)
+
+
+    
+    async def roll(self, context, dice: str):
+        """Rolls a dice in NdN format."""
+        try:
+            rolls, limit = map(int, dice.split("d"))
+        except Exception:
+            return await context.send("Format has to be in NdN!")
+            return
+
+        result = ", ".join(str(randint(1, limit)) for r in range(rolls))
+        return await context.send(result)
+
+
+    #self.interface.command(description="For when you wanna settle the score some other way")
+    async def choose(self, context, *choices: str):
+        """Chooses between multiple choices."""
+        return await context.send(choice(choices))
+
+
+    
+    async def joined(self, context, member):
+        """Says when a member joined."""
+        return await context.send("comando desativado")
+        return await context.send(f"{member.name} joined {discord.utils.format_dt(member.joined_at)}")
+
+
+
+    ######################################################
+
+    
+    async def linux(self, context):
+        """Usa Linux boyy (?)"""
+        return await context.send("Usa Linux boyy")
+        return await context.send(file=discord.File("Tux.svg.png"))
+
+
+    
+    async def desculpa(self, context):
+        """Peço desculpas pelo meu comportamento uwu"""
+        return await context.send("Peço desculpas pelo meu comportamento uwu")
+
+
+    
+    async def repete(self, context, *args, content="Repetindo"):
+        """ " !repete palavras a serem repetidas uma vez"""
+        arguments = " ".join(args)  # .join joins tudo de uma lista, tuple ou dict
+        #    print (arguments)
+        return await context.send(f"{arguments}")
+
+
+    
+    async def repeat(self, context, times: int, content="Repetindo..."):
+        """!repeat (número de vezes a repetir) palavra"""
+        if times <= 8:
+            for i in range(times):
+                return await context.send(content)
+        else:
+            return await context.send(f"Pq vc não fala {content} pro seu birolho {times} vezes?")
+            ofensa = self.curses[randint(1, 70)]
+            return await context.send(f"Tá querendo me banir do discord seu {ofensa}")
+
+
+    #        response= requests.get(request_url)
+    #         return await context.send(xingada)
+
+
+    
+    async def pizza(self, context, left: int, right: int, content="Pizzaiando"):
+        """!pizza numerodefatias raiodapizza
+        use pizzahelp em caso de dúvidas
+        """
+        fatias = int(left)
+        r = int(right)
+
+        b = math.radians((360 / int(fatias)) * 0.5)
+        cossenus = cos(b).real
+        profundidade = round(r - (cossenus * r), 3)
+
+        senus = math.sin(b).real
+        comprimento = round(2 * senus * r, 2)
+        print(comprimento)
+        return await context.send(comprimento)
+
+    
+    async def pizzahelp(self, context):
+        """instrucoes sobre o comando pizza"""
+        saida = "Posicione a régua de forma tangente à borda da pizza. Depois, aproxime o ponto de tangência ao centro\
+            da circunferencia até que o comprimento do segmento da régua que está \"dentro\" da pizza seja igual ao comprimento\
+                fornecido pelo comando pizza. Inicie dois cortes a partir dos 2 pontos onde a borda da régua intercepta a circunferência.\
+                    Agora você tem um pedaço que mede exatamente o necessário!"
+        context.send(saida)
+
+    
+    async def sergio(self, context, description="Quantos dias faltam para SERGIO SALES"):
+        """Quantos dias faltam para SERGIO SALES"""
+        today = str(datetime.datetime.today().strftime("%j"))
+        if today == "217":
+            sergio = "HOJE É O ANIVERSÁRIO DO SÉRGIO MITO (ou o dia seguinte)"
+        elif today == "218":
+            sergio = "HOJE É O ANIVERSÁRIO DO SÉRGIO MITO (ou o dia seguinte)"
+            print(sergio)
+        else:
+            sergio = "Hoje não é o aniverśario de serginho. "
+            if int(today) > 217:
+                daysuntil = 365 - int(today) + 217
+            else:
+                daysuntil = 217 - int(today)
+            print(sergio)
+            print(daysuntil)
+        return await context.send(sergio)
+        return await context.send(f"ainda faltam {daysuntil} dias")
+
+
+    
+    async def enem(self, context):
+        """Quantos dias faltam pro tinhoso dia (motivacional)"""
+        today = int(datetime.datetime.today().strftime("%j"))
+        if today == 317:
+            return await context.send(f"Hoje é o dia do enem :) {self.emojiappended[randint(1, 116)]}")
+        else:
+            return await context.send(
+                f"Faltam {317-today} dias pro ENEM {self.emojiappended[randint(1, 116)]}"
+            )
+
+
+    
+    async def salve(self, context, *args):
+        tosend = self.emojiappended[randint(1, 409)]
+        return await context.send(f"Salve pra {tosend}")
+
+
+    
+    async def corrige(self, context, left: int, mid: str, right: int, content="Corrigindo"):
+        """!corrige o enem 2022: dia-enem(1 ou 2) cor(az(azul), br(branco) etc) questão"""
+        # arguments = ' '.join(args) # .join joins tudo de uma lista, tuple ou dict
+        # cor = arguments[0]   BOT_TOKEN = environ["BOT_TOKEN"]
+
+        # quest = arguments[2]
+        # quest2 = arguments[3]
+        # questf = ''.join(arguments)
+        print(left, mid, right)
+        if left == 1:
+            if mid == "az":
+                return await context.send(self.gabaritoh[right])
+            elif mid == "am":
+                return await context.send(self.gabaritoh[right + 91])
+            elif mid == "br":
+                return await context.send(self.gabaritoh[right + 182])
+            elif mid == "ro":
+                return await context.send(self.gabaritoh[right + 273])
+            else:
+                return await context.send(
+                    "sintaxe: !corrige dia-do-enem(1 ou 2) cor-do-caderno(az(de azul), br(de branco), ro(rosa), am(amarelo)) questão"
+                )
+        elif left == 2:
+            if mid == "am":
+                return await context.send(self.gabaritoe[right])
+            elif mid == "cz":
+                return await context.send(self.gabaritoe[right + 1])
+            elif mid == "az":
+                return await context.send(self.gabaritoe[right + 92])
+            elif mid == "ro":
+                return await context.send(self.gabaritoe[right + 183])
+            else:
+                return await context.send(
+                    "sintaxe: !corrige dia-do-enem(1 ou 2) cor-do-caderno(az(de azul), br(de branco), ro(rosa), am(amarelo)) questão"
+                )
+        else:
+            return await context.send(
+                "sintaxe: !corrige dia-do-enem(1 ou 2) cor-do-caderno(az(de azul), br(de branco), ro(rosa), am(amarelo)) questão"
+            )
+            return await context.send("exemplo: dia 1, prova azul, questão 30: !corrige 1 az 30")
+
+
+    
+    async def nota(self, context, n1: float, n2: float, n3: float):
+        """!nota nota1 nota2 nota3. outputa a nota faltando"""
+        n4 = 28 - n1 - n2 - n3
+        return await context.send(n4)
+
+
+    
+    async def wordle(self, context, wordsize = 5):  
+        if wordsize not in (5,):
+            context.send("Só há suporte para 5 letras")
+        return await context.send(self.wordle5.update())
+        #async for message in context.history(limit=7):
+        #    print(f"{message.author}:  {message.content}")
+
+    
+    async def wt(self, context, guess="NULL"):
+        """wt(ordle try) guess"""
+        return await context.send(self.wordle5.attempt(guess, context.message.author.name))
+        
+    
+    async def wordlewinners(self, context):
+        print("message: " + context.author.name)
+        return await context.send(self.wordle5.winners())
+        
+
+    def getDuplas(*pessoas):
+        people_n = len(pessoas)
+        people_shuffled = sample(pessoas, people_n)
+        for i in range(0, people_n, 2):
+            print(str(people_shuffled[i]),str( people_shuffled[i+1]))
+        
+    # 🄰 🄱 🄲 🄳 🄴 🄵 🄶 🄷 🄸 🄹 🄺 🄻 🄼 🄽 🄾 🄿 🅀 🅁 🅂 🅃 🅄 🅅 🅆 🅇 🅈 🅉
+    #Ⓐ Ⓑ Ⓒ Ⓓ Ⓔ Ⓕ Ⓖ Ⓗ Ⓘ Ⓙ Ⓚ Ⓛ Ⓜ Ⓝ Ⓞ Ⓟ Ⓠ Ⓡ Ⓢ Ⓣ Ⓤ Ⓥ Ⓦ Ⓧ Ⓨ Ⓩ 🅐 🅑 🅒 🅓 🅔 🅕 🅖 🅗 🅘 🅙 🅚 🅛 🅜 🅝 🅞 🅟 🅠 🅡 🅢 🅣 🅤 🅥 🅦 🅧 🅨 🅩
+    #🅰 🅱 🅲 🅳 🅴 🅵 🅶 🅷 🅸 🅹 🅺 🅻 🅼 🅽 🅾 🅿 🆀 🆁 🆂 🆃 🆄 🆅 🆆 🆇 🆈 🆉 🇦 🇧 🇨 🇩 🇪 🇫 🇬 🇭 🇮 🇯 🇰 🇱 🇲 🇳 🇴 🇵 🇶 🇷 🇸 🇹 🇺 🇻 🇼 🇽 🇾 🇿
+    
+    async def sortduplas(self, context, *args):
+        """Arranja todos os parametros em duplas"""
+        Persons = list(args)
+        NumOfPersons = len(Persons)
+        if NumOfPersons == 0:
+            context.send("0 duplas")
+        shuffle(Persons)
+        Persons.append("Com seus pensamentos")
+        Pairs = {}
+        i = 0
+
+        while i < NumOfPersons:
+            Pairs[Persons[i]] = Persons[i + 1]
+            i += 2
+        Output = ""
+        i = 1
+        for person, pair in Pairs.items():
+            Output += f"{i}:   {person} com {pair}\n"
+            i += 1
+        return await context.send(Output)
+
--- main.py
+++ main.py
@@ -51,7 +51,6 @@ if testRequest.status_code != 200:
     sys.exit(1)
 wordle5 = wordleClass(5, words5, postUrl=postUrl, getUrl=getUrl, testUrl=testUrl, apiHeaders=apiHeaders)
 
-
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
@@ -59,248 +58,54 @@ async def on_ready():
     print("------")
 
 
+# -generated with a  macro #
+#############################################
+import Andrebot
+andrebot = Andrebot.Andrebot(print)
 @bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
-    await ctx.send(ctx)
-
-
+async def add(ctx, *args):return await andrebot.add(ctx, *args)
 @bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split("d"))
-    except Exception:
-        await ctx.send("Format has to be in NdN!")
-        return
-
-    result = ", ".join(str(randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-
-@bot.command(description="For when you wanna settle the score some other way")
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(choice(choices))
-
-
+async def choose(ctx, *args):return await andrebot.choose(ctx, *args)
 @bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send(f"{member.name} joined {discord.utils.format_dt(member.joined_at)}")
-
-
-@bot.group()
-async def cool(ctx, *args):
-    """Says if a user is cool.
-    In reality this just checks if a subcommand is being invoked.
-    """
-
-######################################################
-
+async def corrige(ctx, *args):return await andrebot.corrige(ctx, *args)
 @bot.command()
-async def linux(ctx):
-    """Usa Linux boyy (?)"""
-    await ctx.send("Usa Linux boyy")
-    await ctx.send(file=discord.File("Tux.svg.png"))
-
-
+async def desculpa(ctx, *args):return await andrebot.desculpa(ctx, *args)
 @bot.command()
-async def desculpa(ctx):
-    """Peço desculpas pelo meu comportamento uwu"""
-    await ctx.send("Peço desculpas pelo meu comportamento uwu")
-
-
+async def enem(ctx, *args):return await andrebot.enem(ctx, *args)
 @bot.command()
-async def repete(ctx, *args, content="Repetindo"):
-    """ " !repete palavras a serem repetidas uma vez"""
-    arguments = " ".join(args)  # .join joins tudo de uma lista, tuple ou dict
-    #    print (arguments)
-    await ctx.send(f"{arguments}")
-
-
+async def getDuplas(ctx, *args):return await andrebot.getDuplas(ctx, *args)
 @bot.command()
-async def repeat(ctx, times: int, content="Repetindo..."):
-    """!repeat (número de vezes a repetir) palavra"""
-    if times <= 8:
-        for i in range(times):
-            await ctx.send(content)
-    else:
-        await ctx.send(f"Pq vc não fala {content} pro seu birolho {times} vezes?")
-        ofensa = curses[randint(1, 70)]
-        await ctx.send(f"Tá querendo me banir do discord seu {ofensa}")
-
-
-#        response= requests.get(request_url)
-#         await ctx.send(xingada)
-
-
+async def print(ctx, *args):return await andrebot.print(ctx, *args)
 @bot.command()
-async def pizza(ctx, left: int, right: int, content="Pizzaiando"):
-    """!pizza numerodefatias raiodapizza
-    use pizzahelp em caso de dúvidas
-    """
-    fatias = int(left)
-    r = int(right)
-
-    b = math.radians((360 / int(fatias)) * 0.5)
-    cossenus = cos(b).real
-    profundidade = round(r - (cossenus * r), 3)
-
-    senus = math.sin(b).real
-    comprimento = round(2 * senus * r, 2)
-    print(comprimento)
-    await ctx.send(comprimento)
-
+async def joined(ctx, *args):return await andrebot.joined(ctx, *args)
 @bot.command()
-async def pizzahelp(ctx):
-    """instrucoes sobre o comando pizza"""
-    saida = "Posicione a régua de forma tangente à borda da pizza. Depois, aproxime o ponto de tangência ao centro\
-        da circunferencia até que o comprimento do segmento da régua que está \"dentro\" da pizza seja igual ao comprimento\
-            fornecido pelo comando pizza. Inicie dois cortes a partir dos 2 pontos onde a borda da régua intercepta a circunferência.\
-                Agora você tem um pedaço que mede exatamente o necessário!"
-    ctx.send(saida)
-
+async def linux(ctx, *args):return await andrebot.linux(ctx, *args)
 @bot.command()
-async def sergio(ctx, description="Quantos dias faltam para SERGIO SALES"):
-    """Quantos dias faltam para SERGIO SALES"""
-    today = str(datetime.datetime.today().strftime("%j"))
-    if today == "217":
-        sergio = "HOJE É O ANIVERSÁRIO DO SÉRGIO MITO (ou o dia seguinte)"
-    elif today == "218":
-        sergio = "HOJE É O ANIVERSÁRIO DO SÉRGIO MITO (ou o dia seguinte)"
-        print(sergio)
-    else:
-        sergio = "Hoje não é o aniverśario de serginho. "
-        if int(today) > 217:
-            daysuntil = 365 - int(today) + 217
-        else:
-            daysuntil = 217 - int(today)
-        print(sergio)
-        print(daysuntil)
-    await ctx.send(sergio)
-    await ctx.send(f"ainda faltam {daysuntil} dias")
-
-
+async def nota(ctx, *args):return await andrebot.nota(ctx, *args)
 @bot.command()
-async def enem(ctx):
-    """Quantos dias faltam pro tinhoso dia (motivacional)"""
-    today = int(datetime.datetime.today().strftime("%j"))
-    if today == 317:
-        await ctx.send(f"Hoje é o dia do enem :) {emojiappended[randint(1, 116)]}")
-    else:
-        await ctx.send(
-            f"Faltam {317-today} dias pro ENEM {emojiappended[randint(1, 116)]}"
-        )
-
-
+async def pizza(ctx, *args):return await andrebot.pizza(ctx, *args)
 @bot.command()
-async def salve(ctx, *args):
-    tosend = indtratadas[randint(1, 409)]
-    await ctx.send(f"Salve pra {tosend}")
-
-
+async def pizzahelp(ctx, *args):return await andrebot.pizzahelp(ctx, *args)
 @bot.command()
-async def corrige(ctx, left: int, mid: str, right: int, content="Corrigindo"):
-    """!corrige o enem 2022: dia-enem(1 ou 2) cor(az(azul), br(branco) etc) questão"""
-    # arguments = ' '.join(args) # .join joins tudo de uma lista, tuple ou dict
-    # cor = arguments[0]
-    # quest = arguments[2]
-    # quest2 = arguments[3]
-    # questf = ''.join(arguments)
-    print(left, mid, right)
-    if left == 1:
-        if mid == "az":
-            await ctx.send(gabaritoh[right])
-        elif mid == "am":
-            await ctx.send(gabaritoh[right + 91])
-        elif mid == "br":
-            await ctx.send(gabaritoh[right + 182])
-        elif mid == "ro":
-            await ctx.send(gabaritoh[right + 273])
-        else:
-            await ctx.send(
-                "sintaxe: !corrige dia-do-enem(1 ou 2) cor-do-caderno(az(de azul), br(de branco), ro(rosa), am(amarelo)) questão"
-            )
-    elif left == 2:
-        if mid == "am":
-            await ctx.send(gabaritoe[right])
-        elif mid == "cz":
-            await ctx.send(gabaritoe[right + 1])
-        elif mid == "az":
-            await ctx.send(gabaritoe[right + 92])
-        elif mid == "ro":
-            await ctx.send(gabaritoe[right + 183])
-        else:
-            await ctx.send(
-                "sintaxe: !corrige dia-do-enem(1 ou 2) cor-do-caderno(az(de azul), br(de branco), ro(rosa), am(amarelo)) questão"
-            )
-    else:
-        await ctx.send(
-            "sintaxe: !corrige dia-do-enem(1 ou 2) cor-do-caderno(az(de azul), br(de branco), ro(rosa), am(amarelo)) questão"
-        )
-        await ctx.send("exemplo: dia 1, prova azul, questão 30: !corrige 1 az 30")
-
-
+async def repeat(ctx, *args):return await andrebot.repeat(ctx, *args)
 @bot.command()
-async def nota(ctx, n1: float, n2: float, n3: float):
-    """!nota nota1 nota2 nota3. outputa a nota faltando"""
-    n4 = 28 - n1 - n2 - n3
-    await ctx.send(n4)
-
-
+async def repete(ctx, *args):return await andrebot.repete(ctx, *args)
 @bot.command()
-async def wordle(ctx, wordsize = 5):  
-    if wordsize not in (5,):
-        ctx.send("Só há suporte para 5 letras")
-    await ctx.send(wordle5.update())
-    #async for message in ctx.history(limit=7):
-    #    print(f"{message.author}:  {message.content}")
-
+async def roll(ctx, *args):return await andrebot.roll(ctx, *args)
 @bot.command()
-async def wt(ctx, guess="NULL"):
-    """wt(ordle try) guess"""
-    await ctx.send(wordle5.attempt(guess, ctx.message.author.name))
-    
+async def salve(ctx, *args):return await andrebot.salve(ctx, *args)
 @bot.command()
-async def wordlewinners(ctx):
-    print("message: " + ctx.author.name)
-    await ctx.send(wordle5.winners())
-    
-
-def getDuplas(*pessoas):
-    people_n = len(pessoas)
-    people_shuffled = sample(pessoas, people_n)
-    for i in range(0, people_n, 2):
-        print(str(people_shuffled[i]),str( people_shuffled[i+1]))
-    
-# 🄰 🄱 🄲 🄳 🄴 🄵 🄶 🄷 🄸 🄹 🄺 🄻 🄼 🄽 🄾 🄿 🅀 🅁 🅂 🅃 🅄 🅅 🅆 🅇 🅈 🅉
-#Ⓐ Ⓑ Ⓒ Ⓓ Ⓔ Ⓕ Ⓖ Ⓗ Ⓘ Ⓙ Ⓚ Ⓛ Ⓜ Ⓝ Ⓞ Ⓟ Ⓠ Ⓡ Ⓢ Ⓣ Ⓤ Ⓥ Ⓦ Ⓧ Ⓨ Ⓩ 🅐 🅑 🅒 🅓 🅔 🅕 🅖 🅗 🅘 🅙 🅚 🅛 🅜 🅝 🅞 🅟 🅠 🅡 🅢 🅣 🅤 🅥 🅦 🅧 🅨 🅩
-#🅰 🅱 🅲 🅳 🅴 🅵 🅶 🅷 🅸 🅹 🅺 🅻 🅼 🅽 🅾 🅿 🆀 🆁 🆂 🆃 🆄 🆅 🆆 🆇 🆈 🆉 🇦 🇧 🇨 🇩 🇪 🇫 🇬 🇭 🇮 🇯 🇰 🇱 🇲 🇳 🇴 🇵 🇶 🇷 🇸 🇹 🇺 🇻 🇼 🇽 🇾 🇿
+async def sergio(ctx, *args):return await andrebot.sergio(ctx, *args)
 @bot.command()
-async def sortduplas(ctx, *args):
-    """Arranja todos os parametros em duplas"""
-    Persons = list(args)
-    NumOfPersons = len(Persons)
-    if NumOfPersons == 0:
-        ctx.send("0 duplas")
-    shuffle(Persons)
-    Persons.append("Com seus pensamentos")
-    Pairs = {}
-    i = 0
+async def sortduplas(ctx, *args):return await andrebot.sortduplas(ctx, *args)
+@bot.command()
+async def wordle(ctx, *args):return await andrebot.wordle(ctx, *args)
+@bot.command()
+async def wordlewinners(ctx, *args):return await andrebot.wordlewinners(ctx, *args)
+@bot.command()
+async def wt(ctx, *args):return await andrebot.wt(ctx, *args)
+#############################################
 
-    while i < NumOfPersons:
-        Pairs[Persons[i]] = Persons[i + 1]
-        i += 2
-    Output = ""
-    i = 1
-    for person, pair in Pairs.items():
-        Output += f"{i}:   {person} com {pair}\n"
-        i += 1
-    await ctx.send(Output)
-
-# keep_alive()
 
 try:
     BOT_TOKEN = environ["BOT_TOKEN"]
